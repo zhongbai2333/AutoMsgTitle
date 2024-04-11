f6dc7ee7dc43aefd909197cc4e7fa65081fc30f3
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2024-04-11 22:50:31 +0800

    添加只有假人或无人的情况下，关闭data循环直到玩家登录；修复可能的低概率BUG：玩家退出的时候刚好新建player_info档案

--- auto_msg_title/__init__.py
+++ auto_msg_title/__init__.py
@@ -3,36 +3,45 @@ import time
 
 from .config import Config
 
-global __mcdr_server, player_info, stop_status
+global __mcdr_server, player_info, stop_status, online_player_list
 
 
 @new_thread("GetPos")
-def getpos_player():
-    global player_info
+def getpos_player(reload: bool = False):
+    global player_info, online_player_list
     time.sleep(1)
     while True:
-        result = rcon_execute(f"execute as @a run data get entity @s Pos")
-        if result:
-            for i in result.split("]"):
-                if i != "":
+        if player_info or reload:
+            online_player_list = []
+            result = rcon_execute(f"execute as @a run data get entity @s Pos")
+            print(player_info)
+            if result:
+                for i in result.split("]")[:-1]:
                     n_result = i.split()
-                    player_name = n_result[0]
-                    xyz_now = [int(float(n_result[-3][1:-2])), int(float(n_result[-2][:-2])), int(float(n_result[-1][:-1]))]
-                    if player_name in player_info.keys():
-                        if player_info[player_name][0] == xyz_now:
-                            if int(time.time()) - player_info[player_name][1] >= config.afk_time and not player_info[player_name][2]:
-                                player_info[player_name][2] = True
-                                __mcdr_server.say(f"§7{player_name} 开始 AFK")
-                        else:
-                            if player_info[player_name][2]:
-                                __mcdr_server.say(f"§7{player_name} 退出 AFK 共用时 {int(time.time()) - player_info[player_name][1]} 秒")
-                            player_info[player_name] = [xyz_now,int(time.time()),False]
-                    else:
-                        player_info[player_name] = [xyz_now,int(time.time()),False]
+                    online_player_list.append(n_result[0])
+                    edit_player_info(n_result[0], [int(float(n_result[-3][1:-2])), int(float(n_result[-2][:-2])), int(float(n_result[-1][:-1]))])
+                for i in list(set(list(player_info.keys())) - set(online_player_list)):
+                    del player_info[i]
         if not stop_status: 
+            reload = False
             time.sleep(1)
         else:
             break
+
+
+def edit_player_info(player_name: str, xyz_now: list):
+    if player_name in player_info.keys():
+        if player_info[player_name][0] == xyz_now:
+            if int(time.time()) - player_info[player_name][1] >= config.afk_time and not player_info[player_name][2]:
+                player_info[player_name][2] = True
+                __mcdr_server.say(f"§7{player_name} 开始 AFK")
+        else:
+            if player_info[player_name][2]:
+                __mcdr_server.say(f"§7{player_name} 退出 AFK 共用时 {int(time.time()) - player_info[player_name][1]} 秒")
+            player_info[player_name] = [xyz_now,int(time.time()),False]
+    else:
+        if config.bot_prefix not in player_name:
+            player_info[player_name] = [xyz_now,int(time.time()),False]
 
 
 # RCON相关
@@ -56,7 +65,7 @@ def on_load(server: PluginServerInterface, _):
     stop_status = False
     config = __mcdr_server.load_config_simple(target_class=Config)
     if __mcdr_server.is_server_startup():
-        getpos_player()
+        getpos_player(True)
 
 
 def on_unload(_):
@@ -71,7 +80,7 @@ def on_server_startup(_):
 # 在线玩家检测
 def on_player_joined(_, player, __):
     global player_info
-    if player not in player_info.keys():
+    if player not in player_info.keys() and config.bot_prefix not in player:
         player_info[player] = [[0,0,0],int(time.time()),False]
 
 
--- auto_msg_title/config.py
+++ auto_msg_title/config.py
@@ -3,5 +3,6 @@ from typing import Any, Dict, Optional
 from mcdreforged.api.all import PluginServerInterface, Serializable
 
 class Config(Serializable):
-    afk_time: int = 300
     debug: bool = False
+    afk_time: int = 300
+    bot_prefix: str = "bot_"
