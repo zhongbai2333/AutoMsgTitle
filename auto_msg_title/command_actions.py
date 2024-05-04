3835316d0d797644d5184d2d2ed0f80df4df9b33
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2024-05-05 01:29:17 +0800

    list命令制作完成；del命令制作完成

--- auto_msg_title/command_actions.py
+++ auto_msg_title/command_actions.py
@@ -7,6 +7,7 @@ class CommandActions():
         from .storage import JsonDataEditor
         self.__mcdr_server = mcdr_server
         self.permission = permission
+        self.ready_del = ""
         self.create_command()
         self.data_editor = JsonDataEditor(f"{self.__mcdr_server.get_data_folder()}/data.json")
         pass
@@ -25,7 +26,7 @@ class CommandActions():
         builder.command("!!amt msg <region_name> addline <line_msg> <line_number>", self.show_help)
         builder.command("!!amt msg <region_name> delline", self.show_help)
         builder.command("!!amt msg <region_name> delline <line_number>", self.show_help)
-        builder.command("!!amt del <region_name>", self.show_help)
+        builder.command("!!amt del <region_name>", self.del_region)
         builder.command("!!amt info <region_name>", self.show_help)
 
         builder.arg("page_number", Integer)
@@ -46,6 +47,7 @@ class CommandActions():
         self.__mcdr_server.register_help_message('!!amt', '获取AutoMsgTitle插件帮助列表')
 
 
+    # help 列表
     def show_help(self, source: CommandSource):
         help_msg_lines = '''
 --------- MCDR 自动消息插件 v{2} ---------
@@ -74,37 +76,77 @@ class CommandActions():
         source.reply(help_msg_rtext)
     
     
+    # list 命令
     def regions_list(self, source: CommandSource, context: CommandContext):
         if source.get_permission_level() < self.permission['list']:
-            source.reply(f"§4权限不足！你至少需要{self.permission['list']}级及以上！")
+            source.reply(f"§4权限不足！你至少需要 {self.permission['list']} 级及以上！")
             return
-        if context:
-            regions = self.data_editor.list()
-        else:
-            regions = self.data_editor.list()
+        # 制作 list 的 Rtext
+        def get_regions_rtext(regions: dict, page: int):
             regions_rtext = RTextList()
-            regions_rtext.append("--------- 区域列表 第 §61 §f页 ---------\n")
+            regions_rtext.append(f"--------- 区域列表 第 §6{page}/{len(regions) // 5 + 1} §f页 ---------\n")
             num = 0
             for i in regions.keys():
                 num += 1
+                if num <= (page - 1) * 5:
+                    continue
+                elif num > page * 5:
+                    break
                 regions_rtext.append(f"{num}. {i}\n")
                 if regions[i]['shape'] == 0:
                     regions_rtext.append(f"形状：§62D\n")
+                    reload_command = f"!!amt add {i} 2d "
                 else:
                     regions_rtext.append(f"形状：§63D\n")
+                    reload_command = f"!!amt add {i} 3d "
+                reload_command += f"{str(regions[i]['pos']['from'])[1:-1].replace(',','').strip()} {str(regions[i]['pos']['to'])[1:-1].replace(',','').strip()} {regions[i]['dimension_id']} "
+                if regions[i]['msg']['title']:
+                    reload_command += f"[{regions[i]['msg']['title']}]"
+                if regions[i]['msg']['subtitle']:
+                    reload_command += f"({regions[i]['msg']['subtitle']})"
+                if regions[i]['msg']['actionbar']:
+                    reload_command += f"#{regions[i]['msg']['actionbar']}#"
                 regions_rtext.append(f"区域：从 §a{str(regions[i]['pos']['from'])} §f到 §a{str(regions[i]['pos']['to'])}\n")
                 regions_rtext.append(f"维度：§a{regions[i]['dimension_id']}\n")
                 regions_rtext.append(f"消息：\n")
                 regions_rtext.append(f" | 标题：§6{regions[i]['msg']['title']}\n")
                 regions_rtext.append(f" | 副标题：§6{regions[i]['msg']['subtitle']}\n")
                 regions_rtext.append(f" | 动作栏：§6{regions[i]['msg']['actionbar']}\n")
-                regions_rtext.append(f" | 消息栏：§6{regions[i]['msg']['msg']}\n")
-            source.reply(regions_rtext)
+                regions_rtext.append(f" | 消息栏：\n")
+                for j in regions[i]['msg']['msg']:
+                    regions_rtext.append(f" |  | §6{j}\n")
+                    reload_command += f"{j};;"
+                regions_rtext.append("---")
+                regions_rtext.append(RText("§7[↺]§r").c(RAction.suggest_command, reload_command[:-2]).h('重新键入 §7!!amt add §r命令'))
+                regions_rtext.append("---")
+                regions_rtext.append(RText("§6[✏]§r").c(RAction.run_command, f"!!amt msg {i}").h("详细编辑消息栏"))
+                regions_rtext.append("---")
+                regions_rtext.append(RText("§4[✕]§r").c(RAction.run_command, f"!!amt del {i}").h("删除这个区域"))
+                regions_rtext.append("---\n")
+            regions_rtext.append("--------- ")
+            regions_rtext.append(RText("<<").c(RAction.run_command, f"!!amt list {page - 1}").h("前一页"))
+            regions_rtext.append(f" 第 §6{page}/{len(regions) // 5 + 1} §f页 ")
+            regions_rtext.append(RText(">>").c(RAction.run_command, f"!!amt list {page + 1}").h("后一页"))
+            regions_rtext.append(" ---------")
+            return regions_rtext
+        if context:
+            regions = self.data_editor.list()
+            max_page = len(regions) // 5 + 1
+            if context['page_number'] < 1:
+                source.reply("已经是第一页了！")
+            elif context['page_number'] > max_page:
+                source.reply("已经是最后一页了！")
+            else:
+                source.reply(get_regions_rtext(regions, context['page_number']))
+        else:
+            regions = self.data_editor.list()
+            source.reply(get_regions_rtext(regions, 1))
 
     
+    # add 2d 命令
     def add_region_2d(self, source: CommandSource, context: CommandContext):
         if source.get_permission_level() < self.permission['add']:
-            source.reply(f"§4权限不足！你至少需要{self.permission['add']}级及以上！")
+            source.reply(f"§4权限不足！你至少需要 {self.permission['add']} 级及以上！")
             return
         def no_list(text):
             if text:
@@ -114,7 +156,7 @@ class CommandActions():
         title = no_list(re.findall(r'\[([^\]]+)\]', context['msg']))
         subtitle = no_list(re.findall(r'\(([^\)]+)\)', context['msg']))
         actionbar = no_list(re.findall(r'#([^#]+)#', context['msg']))
-        msg = context['msg'].replace(f"[{title}]", '').strip().replace(f"({subtitle})", '').strip().replace(f"#{actionbar}#", '').strip()
+        msg = context['msg'].replace(f"[{title}]", '').strip().replace(f"({subtitle})", '').strip().replace(f"#{actionbar}#", '').strip().split(";;")
         self.data_editor.add(context['region_name'], {
             "shape": 0,
             "pos": {
@@ -126,15 +168,16 @@ class CommandActions():
                 "title": title,
                 "subtitle": subtitle,
                 "actionbar": actionbar,
-                "msg": [msg]
+                "msg": msg
             }
         })
-        source.reply(f"区域 {context['region_name']} 添加(修改)成功！")
+        source.reply(f"区域 §7{context['region_name']} §r添加(修改)成功！")
     
 
+    # add 3d 命令
     def add_region_3d(self, source: CommandSource, context: CommandContext):
         if source.get_permission_level() < self.permission['add']:
-            source.reply(f"§4权限不足！你至少需要{self.permission['add']}级及以上！")
+            source.reply(f"§4权限不足！你至少需要 {self.permission['add']} 级及以上！")
             return
         def no_list(text):
             if text:
@@ -144,7 +187,7 @@ class CommandActions():
         title = no_list(re.findall(r'\[([^\]]+)\]', context['msg']))
         subtitle = no_list(re.findall(r'\(([^\)]+)\)', context['msg']))
         actionbar = no_list(re.findall(r'#([^#]+)#', context['msg']))
-        msg = context['msg'].replace(f"[{title}]", '').strip().replace(f"({subtitle})", '').strip().replace(f"#{actionbar}#", '').strip()
+        msg = context['msg'].replace(f"[{title}]", '').strip().replace(f"({subtitle})", '').strip().replace(f"#{actionbar}#", '').strip().split(";;")
         self.data_editor.add(context['region_name'], {
             "shape": 1,
             "pos": {
@@ -156,7 +199,29 @@ class CommandActions():
                 "title": title,
                 "subtitle": subtitle,
                 "actionbar": actionbar,
-                "msg": [msg]
+                "msg": msg
             }
         })
-        source.reply(f"区域 {context['region_name']} 添加(修改)成功！")
+        source.reply(f"区域 §7{context['region_name']} §r添加(修改)成功！")
+    
+
+    # del 命令
+    def del_region(self, source: CommandSource, context: CommandContext):
+        if source.get_permission_level() < self.permission['del']:
+            source.reply(f"§4权限不足！你至少需要 {self.permission['del']} 级及以上！")
+            return
+        if context['region_name'] not in self.data_editor.list().keys():
+            source.reply(f"§4无法找到区域 {context['region_name']} ！")
+            return
+        if self.ready_del != context['region_name']:
+            self.ready_del = context['region_name']
+            del_msg_rtext = RTextList()
+            del_msg_rtext.append("你§l确定§r要§4§l删除§r此区域吗？\n")
+            del_msg_rtext.append(f"重复输入 §7!!amt del {context['region_name']} §r以确认！")
+            del_msg_rtext.append(RText("§a[✓]").c(RAction.run_command, f"!!amt del {context['region_name']}").h("§a确认删除"))
+            source.reply(del_msg_rtext)
+        else:
+            self.data_editor.remove(self.ready_del)
+            source.reply(f"成功删除区域 §7{self.ready_del} §r！")
+            self.ready_del = ""
+
